861429d115ddf492716eb30b2c854ff81f31ab32
soyrbto <soyrbto@gmail.com>
2025-09-22 16:54:49 -0400

    update

--- internal/gateways/cmd/install.go
+++ internal/gateways/cmd/install.go
@@ -12,48 +12,52 @@ import (
 var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Initialise and configure the application database",
-	Long:  ``,
-	// 1. Cobra calls this **after** parsing the raw CLI tokens
-	//    but **before** normal flag parsing and **before** Run.
-	PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-		// 2. Dynamically add the flags
-		for _, feat := range extensions.Available {
-			if feat.Args == nil {
-				continue
-			}
-			for _, arg := range feat.Args {
-				if arg.Required {
-					cmd.Flags().String(arg.Name, "", arg.Description)
-					_ = cmd.MarkFlagRequired(arg.Name)
-				} else {
-					cmd.Flags().String(arg.Name, arg.Default, arg.Description)
-				}
+
+	PersistentPreRunE: setFlags,
+	Run:               runCmd,
+}
+
+//  1. Cobra calls this **after** parsing the raw CLI tokens
+//     but **before** normal flag parsing and **before** Run.
+func setFlags(cmd *cobra.Command, _ []string) error {
+	// 2. Dynamically add the flags
+	for _, feat := range extensions.Available {
+		if feat.Args == nil {
+			continue
+		}
+		for _, arg := range feat.Args {
+			if arg.Required {
+				cmd.Flags().String(arg.Name, "", arg.Description)
+				_ = cmd.MarkFlagRequired(arg.Name)
+			} else {
+				cmd.Flags().String(arg.Name, arg.Default, arg.Description)
 			}
 		}
-		// 3. Tell Cobra to re-parse the command line with the new flags
-		return cmd.ParseFlags(os.Args[1:])
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		// normal body unchanged
-		setupArgs := make(map[string]map[string]string)
-		for _, feat := range extensions.Available {
-			if feat.Args == nil {
-				continue
-			}
-			if setupArgs[feat.Name] == nil {
-				setupArgs[feat.Name] = make(map[string]string)
-			}
-			for _, arg := range feat.Args {
-				val, _ := cmd.Flags().GetString(arg.Name) // err already checked
-				setupArgs[feat.Name][arg.Name] = val
-			}
+	}
+	// 3. Tell Cobra to re-parse the command line with the new flags
+	return cmd.ParseFlags(os.Args[1:])
+}
+
+func runCmd(cmd *cobra.Command, args []string) {
+	// normal body unchanged
+	setupArgs := make(map[string]map[string]string)
+	for _, feat := range extensions.Available {
+		if feat.Args == nil {
+			continue
 		}
-		if err := extensions.RunMigrations(extensions.Available); err != nil {
-			fmt.Println(err)
-			return
+		if setupArgs[feat.Name] == nil {
+			setupArgs[feat.Name] = make(map[string]string)
 		}
-		extensions.RunSetupPackages(setupArgs)
-	},
+		for _, arg := range feat.Args {
+			val, _ := cmd.Flags().GetString(arg.Name) // err already checked
+			setupArgs[feat.Name][arg.Name] = val
+		}
+	}
+	if err := extensions.RunMigrations(extensions.Available); err != nil {
+		fmt.Println(err)
+		return
+	}
+	extensions.RunSetupPackages(setupArgs)
 }
 
 func init() {
